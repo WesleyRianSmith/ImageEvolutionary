085cbd62a40945ba76b29cfbd58f9bb4ccb803f9
WesleyRianSmith <w.smith7@newcastle.ac.uk>
2023-03-10 10:24:29 +0000

    COmmit

--- main.py
+++ main.py
@@ -6,37 +6,60 @@ MAX = 255 * 200 * 200
 TARGET = Image.open("darwin.png")
 TARGET.load()  # read image and close the file
 
-def mutate(solution, indpb):
-    print("-"* 30)
-    if random.random() < 2:
-        polygon_index = random.randint(0,len(solution)-1)
+
+def mutate(solution, rate):
+    # print("-"* 30)
+    i = random.randint(1,5)
+    if i == 1:
+        # print("Random point change")
+        polygon_index = random.randint(0, len(solution) - 1)
         polygon = solution[polygon_index]
         coords = [x for x in polygon]
-        print(coords)
-        if random.random() < indpb:
-            point_index = random.randint(1, len(coords)-1)
-            random_point = coords[point_index]
-            print(random_point)
-            new_point = tuple([x + random.randint(-5, 5) for x in random_point])
-            print(new_point)
-            coords[point_index] = new_point
-            print(coords)
+        point_index = random.randint(1, len(coords) - 1)
+        random_point = coords[point_index]
+        new_point = tuple([max(10, min(190, int(x + random.gauss(0, 10)))) for x in random_point])
+        coords[point_index] = new_point
         solution[polygon_index] = coords
-
-
+    elif i == 2:
+        # print("Add new polygon")
+        solution.append(make_polygon(random.randint(3, 5)))
+    elif i == 3:
+        # print("Change colour")
+        polygon_index = random.randint(0, len(solution) - 1)
+        polygon = solution[polygon_index]
+        colors = [x for x in polygon[0]]
+        for x in range(3):
+            colors[x] = max(0, min(256, int(colors[x] + random.gauss(0, 10))))
+        polygon[0] = tuple(colors)
+    elif i == 4:
+        # print("shuffling polygons")
+        random.shuffle(solution)
     else:
-        if random.random() < indpb:
-            pass
+        # print("New alpha value")
+        polygon_index = random.randint(0, len(solution) - 1)
+        polygon = solution[polygon_index]
+        colors = [x for x in polygon[0]]
+        new_alpha = max(30, min(60, int(colors[3] + random.gauss(0, 10))))
+        colors[3] = new_alpha
+        polygon[0] = tuple(colors)
         pass
 
     return solution
     pass
 
-def combine():
+
+def combine(*parents):
+    print(*parents)
+    return 2
     pass
 
-def select():
+
+def select(solution):
+    tournament = random.sample(solution, 2)
+    print(tournament)
+    return max(tournament, key=lambda x: x.fitness)
     pass
+
 
 def evaluate(solution):
     image = draw(solution)
@@ -44,6 +67,7 @@ def evaluate(solution):
     hist = diff.convert("L").histogram()
     count = sum(i * n for i, n in enumerate(hist))
     return (MAX - count) / MAX
+
 
 def draw(solution):
     image = Image.new("RGB", (200, 200))
@@ -54,28 +78,39 @@ def draw(solution):
     image.save("solution.png")
     return image
 
+
 def make_polygon(n):
+    # 0 <= R|G|B < 256, 30 <= A <= 60, 10 <= x|y < 190
     color = [random.randint(0, 256) for _ in range(3)]
-    color.append(random.randint(30,60))
+    color.append(random.randint(30, 60))
     color = [tuple(color)]
     coordinates = [(random.randint(10, 190), random.randint(10, 190)) for _ in range(n)]
     return color + coordinates
+
 
 def initialise():
     return [make_polygon(3) for i in range(3)]
 
 
+evolution = (Evolution().survive(fraction=1)
+             .breed(parent_picker=select, combiner=combine)
+             .mutate(mutate_function=mutate, rate=0.1)
+             .evaluate())
+
 
 def run():
+    population = Population.generate(initialise, evaluate, size=20, maximize=True)
+    population = population.evolve(evolution)
+    print(population.individuals)
+    print(select(population.individuals))
+    #for x in population:
+        #print(x.chromosome)
+
+    for i in range(1):
+        draw(population[0].chromosome).save("solution" + str(i) + ".png")
+        population = population.evolve(evolution)
+        print("i =", i, " best =", population.current_best.fitness,
+              " worst =", population.current_worst.fitness)
 
 
-    population = Population.generate(initialise, lambda x: 1, size=10, maximize=True)
-    print(population[0].chromosome)
-    mutate(population[0].chromosome,1)
-    print("-"*30)
-    population_piece = population[0].chromosome
-    print(population_piece)
-    #print(random.choice(population_piece))
-    #draw(population[0].chromosome)
-    #print(evaluate(population[0].chromosome))
 run()
